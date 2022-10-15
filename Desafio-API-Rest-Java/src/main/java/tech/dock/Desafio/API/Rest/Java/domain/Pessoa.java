b818f42b8375d3f99d9d3876386aecee6e013d87
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2022-10-15 09:56:52 -0300

    feature - mapeamento objeto relacional das entidades

--- Desafio-API-Rest-Java/src/main/java/tech/dock/Desafio/API/Rest/Java/domain/Pessoa.java
+++ Desafio-API-Rest-Java/src/main/java/tech/dock/Desafio/API/Rest/Java/domain/Pessoa.java
@@ -2,14 +2,30 @@ package tech.dock.Desafio.API.Rest.Java.domain;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pessoas")
 public class Pessoa {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idPessoa;
+	
 	private String nome;
 	private String cpf;
 	private LocalDate dataNascimento;
 	
+	public Pessoa() {
+		
+	}
+	
 	public Pessoa(Long idPessoa, String nome, String cpf, LocalDate dataNascimento) {
+		super();
 		this.idPessoa = idPessoa;
 		this.nome = nome;
 		this.cpf = cpf;
@@ -46,6 +62,31 @@ public class Pessoa {
 
 	public void setDataNascimento(LocalDate dataNascimento) {
 		this.dataNascimento = dataNascimento;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idPessoa == null) ? 0 : idPessoa.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		if (idPessoa == null) {
+			if (other.idPessoa != null)
+				return false;
+		} else if (!idPessoa.equals(other.idPessoa))
+			return false;
+		return true;
 	}
 	
 }
